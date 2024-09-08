8537d5b4c80ddbfc901141fed4e6bfb9abd0fca1
Jason LeBrun <jblebrun@gmail.com>
2024-09-08 11:38:35 -0700

    Fix layout
    * Remove top bar
    * Remove some unused resources
    * Use simpler imageview for render
    * Organize layout code some more

--- app/src/main/java/com/emerjbl/ultra8/MainActivity.kt
+++ app/src/main/java/com/emerjbl/ultra8/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
@@ -21,9 +21,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,22 +35,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.emerjbl.ultra8.ui.theme.Ultra8Theme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 
-// Wrap the Bitmap in a changing class to trigger updates
+/** Wrap the Bitmap in a class to trigger updates. */
 class BitmapHolder(val bitmap: Bitmap)
 
+/** Pre-loaded program entry */
 data class Program(val name: String, val id: Int)
+
 class MainActivity : ComponentActivity() {
-    val gfx: Chip8Graphics = Chip8Graphics()
-    var machine: Chip8 = Chip8(gfx)
+    private val gfx: Chip8Graphics = Chip8Graphics()
+    private val machine: Chip8 = Chip8(gfx)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +60,7 @@ class MainActivity : ComponentActivity() {
             Program(it.name, it.getInt(null))
         }
 
+        actionBar?.hide()
         enableEdgeToEdge()
         setContent {
             val bitmap = remember { mutableStateOf(BitmapHolder(gfx.b)) }
@@ -85,7 +90,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun Screen(
     bitmapHolder: BitmapHolder,
@@ -94,27 +98,18 @@ fun Screen(
     onKeyDown: (Int) -> Unit,
     onKeyUp: (Int) -> Unit
 ) {
-    var expanded by remember { mutableStateOf(false) }
-    Ultra8Theme {
 
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Ultra8Theme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = { TopBar(programs, onSelectProgram) },
+        ) { innerPadding ->
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(innerPadding)
             ) {
-                Box {
-                    Button(onClick = { expanded = !expanded }) { Text("Programs") }
-                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                        for (program in programs) {
-                            DropdownMenuItem(text = { Text(program.name) }, onClick = {
-                                onSelectProgram(program.id)
-                                expanded = false
-                            })
-                        }
-                    }
-                }
                 Graphics(bitmapHolder)
                 Buttons(onKeyDown, onKeyUp)
             }
@@ -123,19 +118,46 @@ fun Screen(
 }
 
 @Composable
-fun Graphics(bitmapHolder: BitmapHolder) {
-    Canvas(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(2f)
-    ) {
-        val bitmap = bitmapHolder.bitmap
-        val scaleX = size.width / bitmap.width
-        val scaleY = size.height / bitmap.height
-        scale(scaleX, scaleY, Offset.Zero) {
-            drawImage(bitmap.asImageBitmap())
+fun ProgramsDropdown(programs: List<Program>, onSelectProgram: (Int) -> Unit) {
+    var programsExpanded by remember { mutableStateOf(false) }
+    Button(
+        onClick = { programsExpanded = !programsExpanded }
+    ) { Text("Programs") }
+    DropdownMenu(
+        expanded = programsExpanded,
+        onDismissRequest = { programsExpanded = false }) {
+        for (program in programs) {
+            DropdownMenuItem(text = { Text(program.name) }, onClick = {
+                onSelectProgram(program.id)
+                programsExpanded = false
+            })
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(programs: List<Program>, onSelectProgram: (Int) -> Unit) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = { Text("Ultra8") },
+        actions = { ProgramsDropdown(programs, onSelectProgram) },
+    )
+}
+
+@Composable
+fun Graphics(bitmapHolder: BitmapHolder) {
+    Image(
+        bitmap = bitmapHolder.bitmap.asImageBitmap(),
+        contentDescription = "Main Screen",
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(2f),
+        filterQuality = FilterQuality.Low
+    )
 }
 
 @Composable
