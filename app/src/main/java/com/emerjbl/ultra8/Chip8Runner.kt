f070d62bd30a188e78ad66915269bfc05f28b315
Jason LeBrun <jblebrun@gmail.com>
2024-09-14 19:51:26 -0700

    Add some simple speed control
    Low Speed, Hi Speed, and turbo buton

--- app/src/main/java/com/emerjbl/ultra8/Chip8Runner.kt
+++ app/src/main/java/com/emerjbl/ultra8/Chip8Runner.kt
@@ -5,8 +5,12 @@ import kotlin.concurrent.thread
 import kotlin.time.TimeSource
 
 class Chip8Runner(val keys: Chip8Keys, val gfx: Chip8Graphics, val timeSource: TimeSource) {
+    data class Period(val millis: Long, val nanos: Int)
+
     private var machine: Chip8? = null
     var runThread: Thread? = null
+    var period: Period = Period(2, 0)
+    var turbo: Boolean = false
 
     fun load(program: ByteArray) {
         Log.i("Chip8", "Resetting machine")
@@ -36,7 +40,11 @@ class Chip8Runner(val keys: Chip8Keys, val gfx: Chip8Graphics, val timeSource: T
                 var halt: Halt? = null
                 while (halt == null) {
                     halt = machine.step()
-                    Thread.sleep(2)
+                    if (turbo) {
+                        Thread.sleep(0, 10)
+                    } else {
+                        Thread.sleep(period.millis, period.nanos)
+                    }
                 }
                 Log.i("Chip8", "Finished with: ${halt}")
             } catch (ex: InterruptedException) {
