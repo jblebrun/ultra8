1140cd900b3bd840c23913f922bd2a19dba683bd
jblebrun <jblebrun@gmail.com>
2024-09-17 20:44:17 -0700

    Bottom bar (#45)
    * Move Bottom Bar to component
    * Use BottomAppBar

--- app/src/main/java/com/emerjbl/ultra8/ui/screen/MainScreen.kt
+++ app/src/main/java/com/emerjbl/ultra8/ui/screen/MainScreen.kt
@@ -1,23 +1,18 @@
 package com.emerjbl.ultra8.ui.screen
 
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.emerjbl.ultra8.Chip8ViewModel
-import com.emerjbl.ultra8.chip8.runner.Chip8Runner
+import com.emerjbl.ultra8.ui.component.BottomBar
 import com.emerjbl.ultra8.ui.component.Graphics
 import com.emerjbl.ultra8.ui.component.Keypad
 import com.emerjbl.ultra8.ui.component.TopBar
@@ -36,6 +31,14 @@ fun MainScreen(
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = { TopBar(viewModel.programs, onLoadProgram) },
+            bottomBar = {
+                BottomBar(
+                    viewModel::lowSpeed,
+                    viewModel::hiSpeed,
+                    viewModel::turboOn,
+                    viewModel::turboOff
+                )
+            }
         ) { innerPadding ->
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -52,27 +55,7 @@ fun MainScreen(
 
                 Spacer(modifier = Modifier.weight(1f))
 
-                Row() {
-                    Button({ viewModel.period = Chip8Runner.Period(2, 0) }) {
-                        Text("LowSpeed")
-                    }
-                    Spacer(modifier = Modifier.weight(1f))
-                    Box(modifier = Modifier.pointerInput(Unit) {
-                        detectTapGestures(
-                            onPress = {
-                                viewModel.turbo = true
-                                tryAwaitRelease()
-                                viewModel.turbo = false
-                            }
-                        )
-                    }) {
-                        Text("Turbo")
-                    }
-                    Spacer(modifier = Modifier.weight(1f))
-                    Button({ viewModel.period = Chip8Runner.Period(2, 0) }) {
-                        Text("HiSpeed")
-                    }
-                }
+
             }
         }
     }
