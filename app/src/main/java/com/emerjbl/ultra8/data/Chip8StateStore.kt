f26904efb60544de00e702305253362b0c0912da
Jason LeBrun <jblebrun@gmail.com>
2024-10-11 19:11:35 -0700

    Create a separate state store layer, remove details from viewmodel
    Remove the serializer test, since the DataStore test covers it, and some
    fo those types have been hidden.

--- app/src/main/java/com/emerjbl/ultra8/data/Chip8StateStore.kt
+++ app/src/main/java/com/emerjbl/ultra8/data/Chip8StateStore.kt
@@ -7,103 +7,134 @@ import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
 import com.emerjbl.ultra8.chip8.graphics.FrameManager
 import com.emerjbl.ultra8.chip8.machine.Chip8
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.withContext
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
-/** Using a Nullable type causes the DataStore to crash. */
-sealed interface MaybeState {
-    class Yes(val state: Chip8.State) : MaybeState
-    data object No : MaybeState
-}
+/** Manages the save states for Chip8 instances. */
+class Chip8StateStore(
+    private val context: Context,
+    private val scope: CoroutineScope = CoroutineScope((Dispatchers.IO + SupervisorJob()))
+) {
+    /** Returns the last state saved, if there was one. */
+    suspend fun lastSavedState(): Chip8.State? =
+        when (val saved = context.chip8StateStore.data.firstOrNull()) {
+            is MaybeState.Yes -> {
+                saved.state
+            }
 
-/** Serialize Chip8State with a simple DataStream approach. */
-object Chip8StateSerializer : Serializer<MaybeState> {
-    override val defaultValue: MaybeState = MaybeState.No
+            else -> null
+        }
 
-    override suspend fun readFrom(input: InputStream): MaybeState {
-        val dis = DataInputStream(input)
-        return try {
+    /** Save the provided state. Pass `null` to clear the saved state. */
+    suspend fun saveSate(state: Chip8.State?) {
+        context.chip8StateStore.updateData {
+            if (state == null) {
+                MaybeState.No
+            } else {
+                MaybeState.Yes(state)
+            }
+        }
+    }
+
+    private val Context.chip8StateStore: DataStore<MaybeState> by dataStore(
+        fileName = "chip8state.u8b",
+        serializer = Chip8StateSerializer,
+        scope = scope,
+    )
+
+    /** Using a Nullable type causes the DataStore to crash. */
+    private sealed interface MaybeState {
+        class Yes(val state: Chip8.State) : MaybeState
+        object No : MaybeState
+    }
+
+    /** Serialize Chip8State with a simple DataStream approach. */
+    private object Chip8StateSerializer : Serializer<MaybeState> {
+        override val defaultValue: MaybeState = MaybeState.No
+
+        override suspend fun readFrom(input: InputStream): MaybeState {
+            val dis = DataInputStream(input)
+            return try {
+                withContext(Dispatchers.IO) {
+                    Chip8.State(
+                        v = dis.readIntArray(16),
+                        hp = dis.readIntArray(16),
+                        stack = dis.readIntArray(64),
+                        mem = run {
+                            // Support variable memory lengths.
+                            val len = dis.readInt()
+                            dis.readByteArray(len)
+                        },
+                        i = dis.readInt(),
+                        sp = dis.readInt(),
+                        pc = dis.readInt(),
+                        targetPlane = dis.readInt(),
+                        gfx = run {
+                            val hires = dis.readBoolean()
+                            val width = dis.readInt()
+                            val height = dis.readInt()
+                            val data = dis.readIntArray(width * height)
+                            FrameManager(hires, FrameManager.Frame(width, height, data))
+                        }
+                    ).let { MaybeState.Yes(it) }
+                }
+            } catch (e: Exception) {
+                Log.w("Chip8", "Failed to read in the stored Chip8 state", e)
+                MaybeState.No
+            }
+        }
+
+        override suspend fun writeTo(
+            t: MaybeState,
+            output: OutputStream
+        ) {
+            val state = when (t) {
+                MaybeState.No -> {
+                    Log.i("Chip8", "Clearing saved state")
+                    return
+                }
+
+                is MaybeState.Yes -> t.state
+            }
+
+            val dos = DataOutputStream(output)
             withContext(Dispatchers.IO) {
-                Chip8.State(
-                    v = dis.readIntArray(16),
-                    hp = dis.readIntArray(16),
-                    stack = dis.readIntArray(64),
-                    mem = run {
-                        // Support variable memory lengths.
-                        val len = dis.readInt()
-                        dis.readByteArray(len)
-                    },
-                    i = dis.readInt(),
-                    sp = dis.readInt(),
-                    pc = dis.readInt(),
-                    targetPlane = dis.readInt(),
-                    gfx = run {
-                        val hires = dis.readBoolean()
-                        val width = dis.readInt()
-                        val height = dis.readInt()
-                        val data = dis.readIntArray(width * height)
-                        FrameManager(hires, FrameManager.Frame(width, height, data))
-                    }
-                ).let { MaybeState.Yes(it) }
+                dos.writeIntArray(state.v)
+                dos.writeIntArray(state.hp)
+                dos.writeIntArray(state.stack)
+                dos.writeInt(state.mem.size)
+                dos.write(state.mem)
+                dos.writeInt(state.i)
+                dos.writeInt(state.sp)
+                dos.writeInt(state.pc)
+                dos.writeInt(state.targetPlane)
+                dos.writeBoolean(state.gfx.hires)
+                val frame = state.gfx.nextFrame(null)
+                dos.writeInt(frame.width)
+                dos.writeInt(frame.height)
+                dos.writeIntArray(frame.data)
             }
-        } catch (e: Exception) {
-            Log.w("Chip8", "Failed to read in the stored Chip8 state", e)
-            MaybeState.No
-        }
-    }
-
-    override suspend fun writeTo(
-        t: MaybeState,
-        output: OutputStream
-    ) {
-        val state = when (t) {
-            MaybeState.No -> {
-                Log.i("Chip8", "Clearing saved state")
-                return
-            }
-
-            is MaybeState.Yes -> t.state
         }
 
-        val dos = DataOutputStream(output)
-        withContext(Dispatchers.IO) {
-            dos.writeIntArray(state.v)
-            dos.writeIntArray(state.hp)
-            dos.writeIntArray(state.stack)
-            dos.writeInt(state.mem.size)
-            dos.write(state.mem)
-            dos.writeInt(state.i)
-            dos.writeInt(state.sp)
-            dos.writeInt(state.pc)
-            dos.writeInt(state.targetPlane)
-            dos.writeBoolean(state.gfx.hires)
-            val frame = state.gfx.nextFrame(null)
-            dos.writeInt(frame.width)
-            dos.writeInt(frame.height)
-            dos.writeIntArray(frame.data)
+        private fun DataOutputStream.writeIntArray(vals: IntArray) {
+            vals.forEach { writeInt(it) }
         }
+
+        private fun DataInputStream.read(ints: IntArray) {
+            repeat(ints.size) { ints[it] = readInt() }
+        }
+
+        private fun DataInputStream.readIntArray(size: Int) =
+            IntArray(size).apply { read(this) }
+
+        private fun DataInputStream.readByteArray(size: Int) =
+            ByteArray(size).apply { readFully(this) }
     }
-
-    private fun DataOutputStream.writeIntArray(vals: IntArray) {
-        vals.forEach { writeInt(it) }
-    }
-
-    private fun DataInputStream.read(ints: IntArray) {
-        repeat(ints.size) { ints[it] = readInt() }
-    }
-
-    private fun DataInputStream.readIntArray(size: Int) =
-        IntArray(size).apply { read(this) }
-
-    private fun DataInputStream.readByteArray(size: Int) =
-        ByteArray(size).apply { readFully(this) }
 }
-
-val Context.chip8StateStore: DataStore<MaybeState> by dataStore(
-    fileName = "chip8state.u8b",
-    serializer = Chip8StateSerializer
-)
--- testutil/src/main/java/com/emerjbl/ultra8/testutil/Assertion.kt
+++ testutil/src/main/java/com/emerjbl/ultra8/testutil/Assertion.kt
@@ -1,32 +1,26 @@
 package com.emerjbl.ultra8.testutil
 
-import com.emerjbl.ultra8.data.MaybeState
+import com.emerjbl.ultra8.chip8.machine.Chip8
 import strikt.api.Assertion
 import strikt.assertions.contentEquals
 import strikt.assertions.isEqualTo
 
-fun Assertion.Builder<MaybeState>.contentEquals(other: MaybeState): Assertion.Builder<MaybeState> =
-    assert("State contents are the same") { maybeSubject ->
-        if (maybeSubject == MaybeState.No) {
-            strikt.api.expectThat(other).isEqualTo(MaybeState.No)
-        } else {
-            val subject = (maybeSubject as MaybeState.Yes).state
-            val other = (other as MaybeState.Yes).state
-            strikt.api.expectThat(subject.v).describedAs("V: %s")
-                .contentEquals(other.v)
-            strikt.api.expectThat(subject.hp).describedAs("HP: %s")
-                .contentEquals(other.hp)
-            strikt.api.expectThat(subject.stack).describedAs("Stack: %s")
-                .contentEquals(other.stack)
-            strikt.api.expectThat(subject.mem).describedAs("Mem: %s")
-                .contentEquals(other.mem)
-            strikt.api.expectThat(subject.i).describedAs("I: %s")
-                .isEqualTo(other.i)
-            strikt.api.expectThat(subject.sp).describedAs("SP: %s")
-                .isEqualTo(other.sp)
-            strikt.api.expectThat(subject.pc).describedAs("PC: %s")
-                .isEqualTo(other.pc)
-            strikt.api.expectThat(subject.targetPlane).describedAs("TargetPlane: %s")
-                .isEqualTo(other.targetPlane)
-        }
+fun Assertion.Builder<Chip8.State>.contentEquals(other: Chip8.State): Assertion.Builder<Chip8.State> =
+    assert("State contents are the same") { subject ->
+        strikt.api.expectThat(subject.v).describedAs("V: %s")
+            .contentEquals(other.v)
+        strikt.api.expectThat(subject.hp).describedAs("HP: %s")
+            .contentEquals(other.hp)
+        strikt.api.expectThat(subject.stack).describedAs("Stack: %s")
+            .contentEquals(other.stack)
+        strikt.api.expectThat(subject.mem).describedAs("Mem: %s")
+            .contentEquals(other.mem)
+        strikt.api.expectThat(subject.i).describedAs("I: %s")
+            .isEqualTo(other.i)
+        strikt.api.expectThat(subject.sp).describedAs("SP: %s")
+            .isEqualTo(other.sp)
+        strikt.api.expectThat(subject.pc).describedAs("PC: %s")
+            .isEqualTo(other.pc)
+        strikt.api.expectThat(subject.targetPlane).describedAs("TargetPlane: %s")
+            .isEqualTo(other.targetPlane)
     }
