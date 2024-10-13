fb2351a59c1a53542d88dea97558eb9c71d55d78
Jason LeBrun <jblebrun@gmail.com>
2024-10-12 18:34:16 -0700

    Bump to version code 8 version 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.emerjbl.ultra8"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3"
+        versionCode = 8
+        versionName = "0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
