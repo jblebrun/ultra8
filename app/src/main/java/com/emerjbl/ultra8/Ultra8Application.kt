28a1918ebd82c979974f35b6d3cffd71addd2655
jblebrun <jblebrun@gmail.com>
2024-10-19 21:21:39 -0700

    Save cyclesPerTick with programs (#116)

--- app/src/main/java/com/emerjbl/ultra8/Ultra8Application.kt
+++ app/src/main/java/com/emerjbl/ultra8/Ultra8Application.kt
@@ -1,11 +1,8 @@
 package com.emerjbl.ultra8
 
 import android.app.Application
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.emerjbl.ultra8.data.Chip8StateStore
-import com.emerjbl.ultra8.data.HaltTypeConverter
-import com.emerjbl.ultra8.data.IntArrayTypeConverter
 import com.emerjbl.ultra8.data.ProgramStore
 import com.emerjbl.ultra8.data.Ultra8Database
 
@@ -20,18 +17,8 @@ class Ultra8Application : Application() {
         override val chip8StateStore by lazy { Chip8StateStore(db.chip8StateDao()) }
         override val programStore by lazy { ProgramStore(this@Ultra8Application, db.programDao()) }
         override val db by lazy {
-            Room.databaseBuilder(
-                this@Ultra8Application,
-                Ultra8Database::class.java, "ultra8-database"
-            )
-                .apply {
-                    if (BuildConfig.DEBUG) {
-                        fallbackToDestructiveMigration()
-                    }
-                }
-                .createFromAsset("seed.db")
-                .addTypeConverter(IntArrayTypeConverter())
-                .addTypeConverter(HaltTypeConverter()).build()
+            deleteDatabase("ultra8-database")
+            Ultra8Database.newForFile(this@Ultra8Application, "ultra8-database", "seed.db")
         }
     }
 }
--- app/src/main/java/com/emerjbl/ultra8/data/Ultra8Database.kt
+++ app/src/main/java/com/emerjbl/ultra8/data/Ultra8Database.kt
@@ -1,10 +1,33 @@
 package com.emerjbl.ultra8.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.emerjbl.ultra8.BuildConfig
 
 @Database(entities = [Chip8ProgramState::class, Program::class], version = 2)
 abstract class Ultra8Database : RoomDatabase() {
     abstract fun chip8StateDao(): Chip8ProgramStateDao
     abstract fun programDao(): ProgramDao
+
+    companion object {
+        fun newForFile(context: Context, name: String, seed: String): Ultra8Database =
+            Room.databaseBuilder(
+                context,
+                Ultra8Database::class.java,
+                name
+            )
+                .createFromAsset(seed)
+                .buildForApp()
+
+        private fun Builder<Ultra8Database>.buildForApp() = apply {
+            if (BuildConfig.DEBUG) {
+                fallbackToDestructiveMigration()
+            }
+        }
+            .addTypeConverter(IntArrayTypeConverter())
+            .addTypeConverter(HaltTypeConverter())
+            .build()
+    }
 }
