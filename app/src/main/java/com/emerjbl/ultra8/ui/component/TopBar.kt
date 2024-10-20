87da34cca37780ffde6962e6f2d36578e5eb7f8a
Jason LeBrun <jblebrun@gmail.com>
2024-10-20 15:55:33 -0700

    Support as low as SDK 24

--- app/src/main/java/com/emerjbl/ultra8/ui/component/TopBar.kt
+++ app/src/main/java/com/emerjbl/ultra8/ui/component/TopBar.kt
@@ -1,5 +1,6 @@
 package com.emerjbl.ultra8.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
@@ -9,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun TopBar(
@@ -18,6 +21,7 @@ fun TopBar(
 ) {
     val title = if (loadedProgram == null) "Ultra8" else "Ultra8: $loadedProgram"
     TopAppBar(
+        modifier = Modifier.background(Color.Blue),
         navigationIcon = {
             IconButton(onClick = openDrawer) {
                 Icon(Icons.Default.Menu, "Menu")
