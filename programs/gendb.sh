9ec541b8f8b4b28aa24ba8481f31567272bfff8e
jblebrun <jblebrun@gmail.com>
2024-10-21 20:49:43 -0700

    Fix initial DB (#121)

--- programs/gendb.sh
+++ programs/gendb.sh
@@ -4,6 +4,8 @@ set -o pipefail
 
 # shellcheck disable=SC2016
 sqlite3 seed.db "CREATE TABLE Program (name TEXT NOT NULL, builtIn INTEGER NOT NULL, cyclesPerTick INTEGER NOT NULL, data BLOB, PRIMARY KEY(name));"
+sqlite3 seed.db "CREATE TABLE Chip8ProgramState (name TEXT NOT NULL, halt TEXT, vRegisters BLOB NOT NULL, hpRegisters BLOB NOT NULL, stack BLOB NOT NULL, mem BLOB NOT NULL, i INTEGER NOT NULL, sp INTEGER NOT NULL, pc INTEGER NOT NULL, hires INTEGER NOT NULL, targetPlane INTEGER NOT NULL, width INTEGER NOT NULL, height INTEGER NOT NULL, plane1Data BLOB NOT NULL, plane2Data BLOB NOT NULL, PRIMARY KEY(name));"
+
 for file in chip8/*
 do
     name=$(basename "$file")
